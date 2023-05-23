b4a33e035e2386611d909ac421ecfd4d70ffc65c
Oleh <ytox@ukr.net>
2023-05-23 23:25:41 +0300

    add test api for get movies

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,17 @@ import {options, urls} from "./constants";
 
 
 const App = () => {
-    const {params, url} = options;
+    const {url, params, headers} = options;
 
-    axios.get(`${url}${urls.movies}`, {params})
+    axios.get(`${url}${urls.movies}`, {params, headers})
         .then((response) => {
             console.log(response.data);
         })
         .catch((error) => {
             console.error(error);
         });
+
+
 
     return (
         <div>
